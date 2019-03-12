f01053b5e1ea148e9a484f07a52828eaa2ade939
Andy O'Neill <aoneill01@gmail.com>
2019-03-11 20:11:53 -0400

    Add linting, formatting, and logging

--- src/web-server.js
+++ src/web-server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
+const logger = require('../config/winston');
 
 const app = express();
 const port = 3000;
 
 function init() {
-    app.use('/', express.static(path.join(__dirname, '../public')));
+  app.use('/', express.static(path.join(__dirname, '../public')));
 
-    app.listen(port, () => console.log(`Web app listening on http://localhost:${port}`));
+  app.listen(port, () => logger.info(`Web app listening on http://localhost:${port}`));
 }
 
 module.exports = {
-    init
+  init
 };
--- src/websocket-server.js
+++ src/websocket-server.js
@@ -1,28 +1,29 @@
 const WebSocket = require('ws');
+const logger = require('../config/winston');
 
 function init() {
-    console.log('Initializing websocket server');
-    const wss = new WebSocket.Server({ port: 8080 });
+  logger.info('Initializing websocket server');
+  const wss = new WebSocket.Server({ port: 8080 });
 
-    wss.on('connection', function connection(ws) {
-        ws.on('message', function incoming(message) {
-            console.log('received: %s', message);
-        });
-
-        ws.send('something');
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+      logger.info('received: %s', message);
     });
 
-    function broadcast(data) {
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
-    };
+    ws.send('something');
+  });
 
-    setInterval(() => broadcast(new Date().toTimeString()), 1000);
+  function broadcast(data) {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  }
+
+  setInterval(() => broadcast(new Date().toTimeString()), 1000);
 }
 
 module.exports = {
-    init
+  init
 };
